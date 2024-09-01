93b137dda56194debb9f3e459dbfae321355effd
Pierluigi Pesenti <gpantaa@gmail.com>
2024-09-01 14:03:39 +0700

    Update Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,17 @@
 /**  @type {import('@jest/types').Config.ProjectConfig} */
 const config = {
-    transform: {
-        "\\.[jt]sx?$": ["ts-jest", {
-            useESM: true,
-        }],
-    },
-    moduleNameMapper: {
-        "(.+)\\.js": "$1",
-    },
-    extensionsToTreatAsEsm: [".ts"],
+	transform: {
+		'\\.[jt]sx?$': [
+			'ts-jest',
+			{
+				useESM: true,
+			},
+		],
+	},
+	moduleNameMapper: {
+		'(.+)\\.js': '$1',
+	},
+	extensionsToTreatAsEsm: ['.ts'],
 };
 
 export default config;
