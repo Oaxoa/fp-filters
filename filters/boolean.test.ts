4a736a2307d62445632cb874a9ddf7204dc46e85
Pierluigi Pesenti <gpantaa@gmail.com>
2024-11-11 00:40:10 +0700

    Prettier fix

--- filters/boolean.test.ts
+++ filters/boolean.test.ts
@@ -4,21 +4,19 @@ describe('boolean', () => {
 	describe('isTrue', () => {
 		const someBooleans = [true, true, false, true, false];
 
-		it.each([{ input: someBooleans, expected: [true, true, true] }])(
-			'',
-			({ input, expected }) => {
-				expect(input.filter(isTrue)).toEqual(expected);
-			}
-		);
+		it.each([{ input: someBooleans, expected: [true, true, true] }])('', ({ input, expected }) => {
+			expect(input.filter(isTrue)).toEqual(expected);
+		});
 	});
 
 	describe('isTruthy', () => {
 		const someValues = [true, false, 1, 0, '', 'Hello', [], [1, 2], {}, { a: 1 }];
 
-		it.each([
-			{ input: someValues, expected: [true, 1, 'Hello', [], [1, 2], {}, { a: 1 }] },
-		])('', ({ input, expected }) => {
-			expect(input.filter(isTruthy)).toEqual(expected);
-		});
+		it.each([{ input: someValues, expected: [true, 1, 'Hello', [], [1, 2], {}, { a: 1 }] }])(
+			'',
+			({ input, expected }) => {
+				expect(input.filter(isTruthy)).toEqual(expected);
+			}
+		);
 	});
 });
