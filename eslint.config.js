981c2404cc0194c4c5d984c382a9018692f4acec
Pierluigi Pesenti <gpantaa@gmail.com>
2024-11-11 00:32:08 +0700

    Tweak eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,7 @@ export default [
 			'@typescript-eslint/ban-types': 'off',
 			'@typescript-eslint/ban-ts-comment': 'off',
 			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
 		},
 	},
 ];
